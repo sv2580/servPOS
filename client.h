44928ed520e6898fe586639c769a2e0f267feb3a
sv8520 <ver.karcolova@gmail.com>
2021-12-29 17:57:00 +0100

    Initial commit

---
+++ client.h
@@ -0,0 +1,8 @@
+//
+// Created by verka on 29. 12. 2021.
+//
+
+#ifndef SEM_CLIENT_H
+#define SEM_CLIENT_H
+int client(int argc, char *argv[]);
+#endif //SEM_CLIENT_H
---
+++ server.h
@@ -0,0 +1,8 @@
+//
+// Created by verka on 29. 12. 2021.
+//
+
+#ifndef SEM_SERVER_H
+#define SEM_SERVER_H
+int server(int argc, char *argv[]);
+#endif //SEM_SERVER_H
