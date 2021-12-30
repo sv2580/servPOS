3570676ccef3f212d7049be5e1befe8c7bcd3a9c
sv8520 <ver.karcolova@gmail.com>
2021-12-30 17:29:20 +0100

    fdgfgdfg

--- client.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by verka on 29. 12. 2021.
-//
-
-#ifndef SEM_CLIENT_H
-#define SEM_CLIENT_H
-int client(int argc, char *argv[]);
-#endif //SEM_CLIENT_H
--- server.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by verka on 29. 12. 2021.
-//
-
-#ifndef SEM_SERVER_H
-#define SEM_SERVER_H
-int server(int argc, char *argv[]);
-#endif //SEM_SERVER_H
